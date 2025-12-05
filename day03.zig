88b90da098dc10162d1c5d8aa2463d9a22ce33c4
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2025-12-05 13:09:23 +0100

    Unify parts

--- day03.zig
+++ day03.zig
@@ -1,29 +1,34 @@
 const std = @import("std");
 const print = std.debug.print;
 
+fn max_combination(line: []const u8, size: usize) i64 {
+    var t: i64 = 0;
+    var idx: usize = 0;
+    for (0..size) |i| {
+        for (idx + 1..line.len - size + i + 1) |j| {
+            if (line[j] > line[idx]) {
+                idx = j;
+            }
+        }
+        t *= 10;
+        t += line[idx] - '0';
+        idx += 1;
+    }
+    return t;
+}
+
 pub fn solve(allocator: std.mem.Allocator) void {
     print("Day 3\n", .{});
     const data = std.fs.cwd().readFileAlloc(allocator, "data/day03.txt", 1024 * 1024) catch unreachable;
     defer allocator.free(data);
 
+    var part1: i64 = 0;
     var part2: i64 = 0;
     var lines = std.mem.splitScalar(u8, std.mem.trimEnd(u8, data, "\n"), '\n');
     while (lines.next()) |line| {
-        var t: i64 = 0;
-        var idx: usize = 0;
-        const size = 12;
-        for (0..size) |i| {
-            for (idx + 1..line.len - size + i + 1) |j| {
-                if (line[j] > line[idx]) {
-                    idx = j;
-                }
-            }
-            t *= 10;
-            t += line[idx] - '0';
-            idx += 1;
-        }
-        part2 += t;
-        print("{s} {}\n", .{ line, t });
+        part1 += max_combination(line, 2);
+        part2 += max_combination(line, 12);
     }
+    print("  part 1: {}\n", .{part1});
     print("  part 2: {}\n", .{part2});
 }
